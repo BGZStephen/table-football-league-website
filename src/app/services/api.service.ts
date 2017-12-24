f22db58cbaf003278864fb413284fcfb108a1018
Stephen Wright <sjw948@gmail.com>
2017-12-24 10:07:25 +0000

    purge api services not accessible via website

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,127 +9,20 @@ export class ApiService {
 
   baseUrl: string = environment.apiUrl;
   authorization: String = environment.authorization;
-  adminAuthorization: String = environment.adminAuthorization;
 
   constructor(
     private http: HttpClient,
     private router: Router,
   ) {}
 
-  leagues = {
-    get: (id) => {
-      const callParams = {type: 'get', url: `/leagues/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    getAll: () => {
-      const callParams = {type: 'get', url: '/admin/leagues'}
-      return this.apiCall(callParams);
-    },
-
-    create: (league) => {
-      const callParams = {type: 'post', url: '/leagues', body: league}
-      return this.apiCall(callParams);
-    },
-
-    delete: (id) => {
-      const callParams = {type: 'delete', url: `/leagues/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    update: (league) => {
-      const callParams = {type: 'put', url: `/leagues/${league._id}`, body: league}
-      return this.apiCall(callParams);
-    },
-  }
-
-  teams = {
-    get: (id) => {
-      const callParams = {type: 'get', url: `/teams/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    getAll: () => {
-      const callParams = {type: 'get', url: '/admin/teams'}
-      return this.apiCall(callParams);
-    },
-
-    create: (team) => {
-      const callParams = {type: 'post', url: '/teams', body: team}
-      return this.apiCall(callParams);
-    },
-
-    delete: (id) => {
-      const callParams = {type: 'delete', url: `/teams/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    update: (team) => {
-      const callParams = {type: 'put', url: `/teams/${team._id}`, body: team}
-      return this.apiCall(callParams);
-    },
-  }
-
-  fixtures = {
-    get: (id) => {
-      const callParams = {type: 'get', url: `/fixtures/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    getAll: () => {
-      const callParams = {type: 'get', url: '/admin/fixtures'}
-      return this.apiCall(callParams);
-    },
-
-    create: (fixture) => {
-      const callParams = {type: 'post', url: '/fixtures', body: fixture}
-      return this.apiCall(callParams);
-    },
-
-    delete: (id) => {
-      const callParams = {type: 'delete', url: `/fixtures/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    update: (fixture) => {
-      const callParams = {type: 'put', url: `/fixtures/${fixture._id}`, body: fixture}
-      return this.apiCall(callParams);
-    },
-  }
-
   users = {
-    get: (id) => {
-      const callParams = {type: 'get', url: `/private/users/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    getAll: () => {
-      const callParams = {type: 'get', url: '/admin/users'}
-      return this.apiCall(callParams);
-    },
-
     create: (user) => {
       const callParams = {type: 'post', url: '/users', body: user}
       return this.apiCall(callParams);
     },
 
-    delete: (id) => {
-      const callParams = {type: 'delete', url: `/users/${id}`}
-      return this.apiCall(callParams);
-    },
-
-    update: (user) => {
-      const callParams = {type: 'put', url: `/users/${user._id}`, body: user}
-      return this.apiCall(callParams);
-    },
-
     authenticate: (user) => {
       const callParams = {type: 'post', url: '/users/authenticate', body: user}
-      return this.apiCall(callParams);
-    },
-
-    authenticateAdminUser: (user) => {
-      const callParams = {type: 'post', url: '/admin/users/authenticate', body: user}
       return this.apiCall(callParams);
     },
   }
@@ -143,7 +36,7 @@ export class ApiService {
 
   apiCall(callParams) {
     const jwt = localStorage.getItem('token');
-    let headers = new HttpHeaders({'Authorization': `${callParams.authorization || this.authorization}`});
+    let headers = new HttpHeaders({'Authorization': `${this.authorization}`});
     if(jwt) {
       headers = headers.set('token', jwt);
     }
