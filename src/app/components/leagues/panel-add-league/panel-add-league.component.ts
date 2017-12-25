d24294c43ab7585a6567edd5d2b21633918c25dc
Stephen Wright <sjw948@gmail.com>
2017-12-25 21:15:30 +0000

    add-league panel template + styles

---
+++ src/app/components/leagues/panel-add-league/panel-add-league.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-add-league',
+  templateUrl: './panel-add-league.component.html',
+  styleUrls: ['./panel-add-league.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PanelAddLeagueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/dashboard/components/dashboard-leagues/dashboard-leagues.component.ts
+++ src/app/modules/dashboard/components/dashboard-leagues/dashboard-leagues.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DashboardApiService } from 'app/services/dashboard-api.service';
+import { GlobalService } from 'app/services/global.service';
 
 @Component({
   selector: 'app-dashboard-leagues',
   templateUrl: './dashboard-leagues.component.html',
-  styleUrls: ['./dashboard-leagues.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class DashboardLeaguesComponent implements OnInit {
 
-  constructor() { }
+  userId: String = JSON.parse(localStorage.getItem('user'))._id;
+  leagues: Array<object>;
+
+  constructor(
+    private dashboardApi: DashboardApiService,
+    private globalService: GlobalService,
+  ) { }
 
   ngOnInit() {
+    this.dashboardApi.users.getLeagues(this.userId)
+    .subscribe(
+      res => {
+        this.leagues = res;
+        console.log(res);
+      },
+      error => {
+        this.globalService.errorHandler.process(error);
+      }
+    )
   }
 
 }
