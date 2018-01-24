c196c35d05002187c9e616ae2f8672b87914e243
Stephen Wright <sjw948@gmail.com>
2018-01-24 22:09:56 +0000

    tema search styles + 'my teams' call

--- src/app/components/panels/teams/panel-search-teams/panel-search-teams.component.ts
+++ src/app/components/panels/teams/panel-search-teams/panel-search-teams.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardApiService } from 'app/services/dashboard-api.service';
+import { GlobalService } from 'app/services/global.service';
 
 @Component({
   selector: 'app-panel-search-teams',
   templateUrl: './panel-search-teams.component.html',
-  styleUrls: ['./panel-search-teams.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class PanelSearchTeamsComponent implements OnInit {
 
-  constructor() { }
+  searchResults: Array<object> = [];
+
+  constructor(
+    private dashboardApi: DashboardApiService,
+    private globalService: GlobalService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  getMyTeams() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    this.dashboardApi.users.get(user._id, {
+      teams: true,
+    })
+    .subscribe(
+      res => {
+        this.searchResults = res.teams;
+      },
+      error => {
+        this.globalService.errorHandler.process(error);
+      }
+    )
   }
 
 }
